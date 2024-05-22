dbdccc4754815ac393290a253012b2fc9d958bf0
NobodyForNothing <82763757+NobodyForNothing@users.noreply.github.com>
2024-05-22 11:14:41 +0200

    implement differentiated line counting

---
+++ src/lang_stats.rs
@@ -0,0 +1,153 @@
+use std::{fs, io};
+use std::io::{BufRead, BufReader};
+use std::path::Path;
+use once_cell::sync::Lazy;
+use regex::Regex;
+use crate::language::Language;
+use crate::language::Language::OTHER;
+
+/// Detailed statistics on source code.
+pub struct LangStats {
+    lang: Language,
+    // Total counts are maintained to be able to verify data consistency.
+    total_files: u64,
+    code_files: u64,
+    test_files: u64,
+    total_lines: u128,
+    /// Number of non-test lines of code. (includes comments or empty)
+    code_lines: u128,
+    /// Number of non-empty comment lines of code outside tests.
+    code_comment_lines: u128,
+    /// Number of empty lines of code outside tests.
+    code_empty_lines: u128,
+    /// Lines of code in tests (includes comments and empty).
+    test_lines: u128,
+    /// Number of non-empty comment lines of code in tests.
+    test_comment_lines: u128,
+    /// Number of empty lines of code in tests.
+    test_empty_lines: u128,
+}
+
+impl LangStats {
+    /// Counts stats in a file and adds them.
+    ///
+    /// When the file format doesn't match or the  no stats are added and false is returned.
+    pub fn add<>(&mut self, path: &str) -> bool {
+        if self.lang == Language::new(&path) {
+            self.total_files += 1;
+            match self.lang {
+                Language::JAVA => self.analyze_java(path),
+                Language::KOTLIN => self.analyze_java(path),
+                Language::C => self.analyze_java(path),
+                Language::CPP => self.analyze_java(path),
+                Language::RUST => self.analyze_java(path), // FIXME: ignoring in file tests
+                Language::PYTHON => self.analyze_bash(path), // FIXME: ignoring multiline string comments and tests
+                Language::GRADLE => self.analyze_java(path),
+                Language::CMAKE => self.analyze_bash(path), // https://cmake.org/cmake/help/v3.1/manual/cmake-language.7.html#comments
+                Language::MAKEFILE => self.analyze_bash(path),
+                Language::ASSEMBLY => self.analyze_general(path, CommentStyle::UNKNOWN), // TODO: implement
+                OTHER(_) => self.analyze_general(path, CommentStyle::UNKNOWN),
+            }
+            true
+        } else {
+            false
+        }
+    }
+
+    /// Analyze a undifferentiated file.
+    fn analyze_general(&mut self, path: &str, comments: CommentStyle) {
+        let lines = count_differentiated_lines(&path, comments);
+        if path.contains("test") {
+            self.test_files += 1;
+            if let Some(lines) = lines {
+                self.test_lines += lines.0 as u128;
+                self.test_comment_lines += lines.1 as u128;
+                self.test_empty_lines += lines.2 as u128;
+            }
+            // Other line types can't be differentiated.
+        } else {
+            self.code_files += 1;
+            if let Some(lines) = lines {
+                self.code_lines += lines.0 as u128;
+                self.code_comment_lines += lines.1 as u128;
+                self.code_empty_lines += lines.2 as u128;
+            }
+        }
+    }
+
+    fn analyze_java(&mut self, path: &str) {
+        self.analyze_general(path, CommentStyle::C)
+    }
+
+    fn analyze_bash(&mut self, path: &str) {
+        let lines = count_differentiated_lines(path, CommentStyle::BASH);
+        self.code_files += 1;
+        if let Some(lines) = lines {
+            self.code_lines += lines.0 as u128;
+            self.code_comment_lines += lines.1 as u128;
+            self.code_empty_lines += lines.2 as u128;
+        }
+    }
+}
+
+enum CommentStyle {
+    // Comments between "#" and "\n".
+    BASH,
+    // Comments between "//" and "\n or "/*" and "*/".
+    C,
+    // No comment counting.
+    UNKNOWN,
+}
+
+/// Counts lines of code in a single file.
+fn count_lines<P: AsRef<Path>>(path: P) -> Result<usize, io::Error> {
+    let handle = fs::File::open(path).unwrap();
+    let mut reader = BufReader::with_capacity(1024 * 32, handle);
+    let mut count = 0;
+    loop {
+        let len = {
+            let buf = reader.fill_buf()?;
+            if buf.is_empty() {
+                break;
+            }
+            count += bytecount::count(&buf, b'\n');
+            buf.len()
+        };
+        reader.consume(len);
+    }
+    Ok(count)
+}
+
+/// Counts all_lines, comment_lines and empty_lines in a [file].
+///
+/// Counts are returned in the aforementioned order.
+///
+/// Comment borders examples:
+/// "//" -> "\n"
+/// "/*" -> "*/"
+fn count_differentiated_lines(file: &str, comments: CommentStyle) -> Option<(usize, usize, usize)> {
+    static EMPTY_LINES_RE: Lazy<Regex> = Lazy::new(|| Regex::new(r"\n[\s\t\r]*\n").unwrap());
+    static BASH_COMMENT_RE: Lazy<Regex> = Lazy::new(|| Regex::new(r"\n\s*#.*\n").unwrap());
+    static C_COMMENT_SINGLE_RE: Lazy<Regex> = Lazy::new(|| Regex::new(r"\n\s*//.*\n").unwrap());
+    static C_COMMENT_MULTI_RE: Lazy<Regex> = Lazy::new(|| Regex::new(r"/\*(?:.|\n)*?\*/").unwrap());
+
+    let content = fs::read_to_string(file).ok()?;
+
+    let total_lines = content.matches("\n").count();
+    let empty_lines = EMPTY_LINES_RE.find_iter(&content).count();
+    let comment_lines: usize = match comments {
+        CommentStyle::BASH => {
+            BASH_COMMENT_RE.find_iter(&content).count()
+        },
+        CommentStyle::C => {
+            let multi_comments: usize = C_COMMENT_MULTI_RE
+                .find_iter(&content)
+                .map(|m| m.as_str().matches('\n').count())
+                .sum();
+            C_COMMENT_SINGLE_RE.find_iter(&content).count() + multi_comments
+        },
+        CommentStyle::UNKNOWN => 0,
+    };
+
+    Some((total_lines, comment_lines, empty_lines))
+}
---
+++ src/language.rs
@@ -0,0 +1,52 @@
+use crate::language::Language::OTHER;
+
+/// Common supported programming languages indicated by file extensions.
+pub enum Language {
+    JAVA,
+    KOTLIN,
+    C,
+    CPP,
+    RUST,
+    PYTHON,
+    GRADLE,
+    CMAKE,
+    MAKEFILE,
+    ASSEMBLY,
+    OTHER(String),
+    // TODO: toml, yaml, md ...
+}
+
+impl Language {
+    /// Finds the language matching to the extension in the [file_name].
+    pub fn new(file_name: &str) -> Language {
+        let extension = file_name
+            .split(".")
+            .last()
+            .expect("split always has last");
+        // No lower case possible because c and cpp
+        match extension {
+            "java" => Language::JAVA,
+            "kt" => Language::KOTLIN,
+            "c" | "h" => Language::C,
+            "cpp" | "CPP" | "cc" | "c++" | "cxx" | "CXX" | "hpp"
+                | "hxx" | "Hxx" | "HXX" | "C" | "H" => Language::CPP,
+            "rs" => Language::RUST,
+            "py" | "py3" | "pxd" | "pyi" | "pyz" | "pywz" | "ipynb" => Language::PYTHON,
+            "gradle" => Language::GRADLE,
+            "cmake" => Language::CMAKE,
+            "mk" | "MK" | "makefile" | "MAKEFILE" => Language::MAKEFILE,
+            "s" | "S" | "asm" => Language::ASSEMBLY,
+            _ => OTHER(extension.to_string())
+        }
+    }
+}
+
+impl PartialEq for Language {
+    fn eq(&self, other: &Self) -> bool {
+        self == other
+    }
+}
+
+// File extension resources:
+// - https://www.openoffice.org/dev_docs/source/file_extensions.html
+// - https://dcjtech.info/topic/python-file-extensions/
